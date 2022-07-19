c95d6905f4bdc735bac8a61be8a23ed9e36b20e0
mateus santos <msouza954@gmail.com>
2022-07-19 11:00:11 -0300

    nav

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,20 @@
 import { HeaderContainer } from './styles'
+import { Scroll, Timer } from 'phosphor-react'
 
 import logoIgnite from '../../assets/logo-ignite.svg'
+import { NavLink } from 'react-router-dom'
 
 export function Header() {
   return (
     <HeaderContainer>
       <img src={logoIgnite} alt="Um triangulo sobreposto sobre o outro" />
       <nav>
-        <a href="">Timer</a>
-        <a href="">history</a>
+        <NavLink to="/" title="Timer">
+          <Timer size={24} />
+        </NavLink>
+        <NavLink to="/history" title="historico">
+          <Scroll size={24} />
+        </NavLink>
       </nav>
     </HeaderContainer>
   )
