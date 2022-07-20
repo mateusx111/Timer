4f4e0dce518813196dec436c719fad272a8f1c23
mateus santos <msouza954@gmail.com>
2022-07-20 09:15:41 -0300

    nav

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,14 @@
+export function Home() {
+  return (
+    <div>
+      <form action="">
+        {
+          <label htmlFor="">
+            Vou trabalhar em
+          </label> /* When a user clicks or touches/taps a label, 
+                  the browser passes the focus to its associated input  */
+        }
+      </form>
+    </div>
+  )
+}
